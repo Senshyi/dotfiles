df5c60cf43bb07b6e4de8041285a0966f73f2c0b
Knajf <janmachacek97@gmail.com>
2023-12-30 22:03:19 +0100

    move wezterm config

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,12 @@
+local wezterm = require("wezterm")
+
+return {
+	enable_tab_bar = false,
+	font_size = 18.0,
+	font = wezterm.font("Berkeley Mono"),
+	color_scheme = "Rosé Pine (Gogh)",
+	enable_scroll_bar = false,
+	scrollback_lines = 10500,
+	min_scroll_bar_height = "1cell",
+	window_decorations = "RESIZE"
+}
