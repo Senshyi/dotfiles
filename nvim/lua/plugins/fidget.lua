9f6ce3c409b550e29a16274d6f1408bab59602ab
ayhonz <janmachacek97@gmail.com>
2024-01-30 11:37:55 +0100

    remove wrong option

--- nvim/lua/plugins/fidget.lua
+++ nvim/lua/plugins/fidget.lua
@@ -1,15 +1,10 @@
 return {
   {
     'j-hui/fidget.nvim',
-    tag = 'legacy',
     event = { 'BufEnter' },
     config = function()
       -- Turn on LSP, formatting, and linting status and progress information
-      require('fidget').setup {
-        text = {
-          spinner = 'dots_negative',
-        },
-      }
+      require('fidget').setup {}
     end,
   },
 }
