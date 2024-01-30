57f1474d71a9dc0d3093e094acec74025caae4ba
ayhonz <janmachacek97@gmail.com>
2024-01-30 13:27:29 +0100

    harpoon setup with keybinds

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -1,6 +1,11 @@
 return {
   {
     'ThePrimeagen/harpoon',
+branch = "harpoon2",
     lazy = true,
+dependencies = { "nvim-lua/plenary.nvim" },
+    cofig = function()
+      require('harpoon').setup()
+    end,
   },
 }
