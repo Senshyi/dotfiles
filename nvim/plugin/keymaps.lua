7cabfcce7f1723054a335060737b8224e92006a3
ayhonz <janmachacek97@gmail.com>
2024-05-19 12:38:30 +0200

    basic keymaps

---
+++ nvim/plugin/keymaps.lua
@@ -0,0 +1,18 @@
+local set = vim.keymap.set
+
+-- Toggle hlsearch if it's on, otherwise just do "enter"
+set("n", "<CR>", function()
+  ---@diagnostic disable-next-line: undefined-field
+  if vim.opt.hlsearch:get() then
+    vim.cmd.nohl()
+    return ""
+  else
+    return "<CR>"
+  end
+end, { expr = true })
+
+-- diagnostic
+set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to previous [D]iagnostic message" })
+set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to next [D]iagnostic message" })
+set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
+set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
