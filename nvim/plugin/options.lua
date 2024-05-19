a23d65aab301e53962ad8f2b25d17220bed1467f
ayhonz <janmachacek97@gmail.com>
2024-05-19 11:12:27 +0200

    add nice options I can't live without :)

--- nvim/plugin/options.lua
+++ nvim/plugin/options.lua
@@ -6,6 +6,14 @@ opt.inccommand = "split"
 opt.smartcase = true
 opt.ignorecase = true
 
+-- For resizing splits etc until I make keybinds
+opt.mouse = "a"
+
+opt.showmode = false
+opt.breakindent = true
+
+opt.undofile = true
+
 -- Must have
 opt.number = true
 opt.relativenumber = true
@@ -13,8 +21,13 @@ opt.relativenumber = true
 -- personal pref
 opt.splitbelow = true
 opt.splitright = true
-
+opt.scrolloff = 8
+opt.cursorline = true
+-- this shows trailing spaces tabs etc
+opt.list = true
+opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 opt.signcolumn = "yes"
+opt.hlsearch = true
 
 -- because I close nvim too many times on accident
 opt.shada = { "'10", "<0", "s10", "h" }
@@ -22,5 +35,4 @@ opt.shada = { "'10", "<0", "s10", "h" }
 opt.clipboard = "unnamedplus"
 
 -- Don't have `o` add a comment
-
 opt.formatoptions:remove "o"
