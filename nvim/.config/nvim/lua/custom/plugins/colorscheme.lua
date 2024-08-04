95f21c7db2aaf5c8509ae5661bad2b7f80f52c62
ayhonz <janmachacek97@gmail.com>
2024-08-04 19:47:47 +0200

    switch to gruvbox :)

--- nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
@@ -4,7 +4,16 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme "kanagawa"
+      -- vim.cmd.colorscheme "kanagawa"
+    end,
+  },
+  {
+    "sainnhe/gruvbox-material",
+    priority = 1000,
+    config = function()
+      vim.g.gruvbox_material_background = "hard"
+      vim.g.gruvbox_material_enable_italic = true
+      vim.cmd.colorscheme "gruvbox-material"
     end,
   },
 }
